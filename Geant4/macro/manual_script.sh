e1b48640d301de83e23f8ed8fa833286f3c3004d
keiyinau <eltisau@outlook.com>
2025-12-02 08:13:40 +0800

    update plastic scint sim result

--- Geant4/macro/manual_script.sh
+++ Geant4/macro/manual_script.sh
@@ -2,7 +2,7 @@
 mv output_0.root Co60_Nearest.root
 ./sim run_t20_100k_Cs137.mac >> output_Cs137.log
 mv output_0.root Cs137_Nearest.root
-#./sim run_t20_100k.mac >> output_Na22.log
-#mv output_0.root Na22_nearest.root
+./sim run_t20_100k.mac >> output_Na22.log
+mv output_0.root Na22_nearest.root
 #./sim cosmic_muon.mac >> output_cosmic.log
 #mv output_0.root Cosmic_nearest.root
